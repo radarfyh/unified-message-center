f35a80bfcc8dbc076830a40fd08be31dfd87f6a6
radarfyh <radarfyh@gmail.com>
2026-04-16 17:55:48 +0800

    app/AppKeyAuthenticationToken.java 移到 feng-library3-security

--- feng-base/feng-auth3/src/main/java/ltd/huntinginfo/feng/auth/support/base/app/AppKeyAuthenticationToken.java
+++ feng-base/feng-auth3/src/main/java/ltd/huntinginfo/feng/auth/support/base/app/AppKeyAuthenticationToken.java
@@ -18,91 +18,91 @@
  *  same license terms as the original work, with additional modifications
  *  as noted above.
  */
-package ltd.huntinginfo.feng.auth.support.base.app;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
-
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
-
-/**
- * APPKEY认证模式认证令牌管理
- * @author radarfyh
- * @date 2026/01/30
- */
-public class AppKeyAuthenticationToken extends AbstractAuthenticationToken {
-	private static final long serialVersionUID = 1L;
-	
-	private final OAuth2ClientAuthenticationToken clientPrincipal;
-    private final Set<String> scopes;
-    private final Map<String, Object> additionalParameters;
-    
-    private String appKey;
-    private String appSecret;
-    private Long timestamp;
-    private String nonce;
-    private String bodyMd5;
-    private String signature;
-
-    public AppKeyAuthenticationToken(OAuth2ClientAuthenticationToken clientPrincipal,
-                                      Set<String> scopes,
-                                      Map<String, Object> additionalParameters) {
-        super(Collections.emptyList());
-        this.clientPrincipal = clientPrincipal;
-        this.scopes = scopes;
-        this.additionalParameters = additionalParameters;
-        setAuthenticated(true);
-        
-        this.appKey = null;
-        this.appSecret = null;
-        this.timestamp = Long.parseLong((String) additionalParameters.getOrDefault("timestamp", ""));
-        this.nonce = (String) additionalParameters.getOrDefault("nonce", "");
-        this.bodyMd5 = (String) additionalParameters.getOrDefault("bodyMd5", "");
-        this.signature = (String) additionalParameters.getOrDefault("signature", "");
-    }
-
-    @Override
-    public Object getPrincipal() {
-        return this.clientPrincipal;
-    }
-
-    @Override
-    public Object getCredentials() {
-        return "";
-    }
-
-    // getters...
-    public Set<String> getScopes() {
-    	return scopes;
-    }
-    
-    public Map<String, Object> getAdditionalParameters() {
-    	return additionalParameters;
-    }
-    
-    public String getAppKey() {
-    	return appKey;
-    }
-    
-    public String getAppSecret() {
-    	return appSecret;
-    }
-    
-    public Long getTimestamp() {
-    	return timestamp;
-    }
-    
-    public String getNonce() {
-    	return nonce;
-    }
-    
-    public String getBodyMd5() {
-    	return bodyMd5;
-    }
-    
-    public String getSignature() {
-    	return signature;
-    }
-}
+//package ltd.huntinginfo.feng.auth.support.base.app;
+//
+//import java.util.Collections;
+//import java.util.Map;
+//import java.util.Set;
+//
+//import org.springframework.security.authentication.AbstractAuthenticationToken;
+//import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
+//
+///**
+// * APPKEY认证模式认证令牌管理
+// * @author radarfyh
+// * @date 2026/01/30
+// */
+//public class AppKeyAuthenticationToken extends AbstractAuthenticationToken {
+//	private static final long serialVersionUID = 1L;
+//	
+//	private final OAuth2ClientAuthenticationToken clientPrincipal;
+//    private final Set<String> scopes;
+//    private final Map<String, Object> additionalParameters;
+//    
+//    private String appKey;
+//    private String appSecret;
+//    private Long timestamp;
+//    private String nonce;
+//    private String bodyMd5;
+//    private String signature;
+//
+//    public AppKeyAuthenticationToken(OAuth2ClientAuthenticationToken clientPrincipal,
+//                                      Set<String> scopes,
+//                                      Map<String, Object> additionalParameters) {
+//        super(Collections.emptyList());
+//        this.clientPrincipal = clientPrincipal;
+//        this.scopes = scopes;
+//        this.additionalParameters = additionalParameters;
+//        setAuthenticated(true);
+//        
+//        this.appKey = null;
+//        this.appSecret = null;
+//        this.timestamp = Long.parseLong((String) additionalParameters.getOrDefault("timestamp", ""));
+//        this.nonce = (String) additionalParameters.getOrDefault("nonce", "");
+//        this.bodyMd5 = (String) additionalParameters.getOrDefault("bodyMd5", "");
+//        this.signature = (String) additionalParameters.getOrDefault("signature", "");
+//    }
+//
+//    @Override
+//    public Object getPrincipal() {
+//        return this.clientPrincipal;
+//    }
+//
+//    @Override
+//    public Object getCredentials() {
+//        return "";
+//    }
+//
+//    // getters...
+//    public Set<String> getScopes() {
+//    	return scopes;
+//    }
+//    
+//    public Map<String, Object> getAdditionalParameters() {
+//    	return additionalParameters;
+//    }
+//    
+//    public String getAppKey() {
+//    	return appKey;
+//    }
+//    
+//    public String getAppSecret() {
+//    	return appSecret;
+//    }
+//    
+//    public Long getTimestamp() {
+//    	return timestamp;
+//    }
+//    
+//    public String getNonce() {
+//    	return nonce;
+//    }
+//    
+//    public String getBodyMd5() {
+//    	return bodyMd5;
+//    }
+//    
+//    public String getSignature() {
+//    	return signature;
+//    }
+//}
